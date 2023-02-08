e602c603732e6cebf40e1a53aa87feacaff41577
Sal Rahman <salehen.rahman@gmail.com>
2023-02-07 18:28:28 -0800

    SvgWrapper implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -220,51 +220,42 @@ function App() {
 						return;
 					}
 				}}
+				onWheel={(e) => {
+					if (!svgRef.current) return;
+
+					const rect = svgRef.current.getBoundingClientRect();
+					const dimensions = [rect.width, rect.height] satisfies Vector2;
+
+					if (e.ctrlKey) {
+						const newZoom = camera.zoom.addLogarithmic(-e.deltaY * 0.01);
+
+						const cursorCenter = start(mousePosition)
+							._((pos) => sub2(pos, scalarMul2(dimensions, 0.5)))
+							._((pos) => hadamard2(pos, [1, -1])).value;
+
+						const newCameraPosition = start(cursorCenter)
+							._((pos) => add2(pos, camera.position))
+							._((pos) => scalarMul2(pos, newZoom.linear / camera.zoom.linear))
+							._((pos) => sub2(pos, cursorCenter)).value;
+
+						updateCamera({
+							zoom: newZoom,
+							position: newCameraPosition,
+						});
+					} else {
+						const delta = [e.deltaX, -e.deltaY] satisfies Vector2;
+
+						updateCamera({
+							position: add2(camera.position, delta),
+						});
+					}
+				}}
 				ref={(ref) => {
 					if (ref === null) {
 						return;
 					}
 					const currentRef = svgRef.current;
 					svgRef.current = ref;
-
-					svgRef.current.addEventListener(
-						"wheel",
-						(e) => {
-							e.preventDefault();
-
-							if (!svgRef.current) return;
-
-							const rect = svgRef.current.getBoundingClientRect();
-							const dimensions = [rect.width, rect.height] satisfies Vector2;
-
-							if (e.ctrlKey) {
-								const newZoom = camera.zoom.addLogarithmic(-e.deltaY * 0.01);
-
-								const cursorCenter = start(mousePosition)
-									._((pos) => sub2(pos, scalarMul2(dimensions, 0.5)))
-									._((pos) => hadamard2(pos, [1, -1])).value;
-
-								const newCameraPosition = start(cursorCenter)
-									._((pos) => add2(pos, camera.position))
-									._((pos) =>
-										scalarMul2(pos, newZoom.linear / camera.zoom.linear)
-									)
-									._((pos) => sub2(pos, cursorCenter)).value;
-
-								updateCamera({
-									zoom: newZoom,
-									position: newCameraPosition,
-								});
-							} else {
-								const delta = [e.deltaX, -e.deltaY] satisfies Vector2;
-
-								updateCamera({
-									position: add2(camera.position, delta),
-								});
-							}
-						},
-						{ passive: false }
-					);
 
 					if (currentRef === ref) return;
 
--- frontend/src/SvgBasic.tsx
+++ frontend/src/SvgBasic.tsx
@@ -1,11 +1,16 @@
-import { forwardRef, useEffect } from "react";
+import { forwardRef, useEffect, useRef } from "react";
 
 export type SvgBasicProps = Omit<React.SVGProps<SVGSVGElement>, "onMouseUp"> & {
 	onMouseUp: () => void;
 };
 
 export const SvgBasic = forwardRef<SVGSVGElement, SvgBasicProps>(
-	({ onMouseDown, onMouseMove, onMouseUp, ...props }, ref) => {
+	(
+		{ onMouseDown, onMouseMove, onMouseUp, onWheel, ...props },
+		forwardedRef
+	) => {
+		const svgRef = useRef<SVGSVGElement | null>(null);
+
 		useEffect(() => {
 			document.addEventListener("mouseup", onMouseUp);
 
@@ -16,7 +21,29 @@ export const SvgBasic = forwardRef<SVGSVGElement, SvgBasicProps>(
 
 		return (
 			<svg
-				ref={ref}
+				ref={(ref) => {
+					if (typeof forwardedRef === "function") {
+						forwardedRef(ref);
+					} else if (forwardedRef) {
+						forwardedRef.current = ref;
+					}
+					if (ref === null) {
+						return;
+					}
+					svgRef.current = ref;
+
+					svgRef.current.addEventListener(
+						"wheel",
+						(e) => {
+							e.preventDefault();
+
+							if (!svgRef.current) return;
+
+							if (onWheel) onWheel(e as any as WheelEvent);
+						},
+						{ passive: false }
+					);
+				}}
 				onMouseDown={onMouseDown}
 				onMouseMove={onMouseMove}
 				{...props}
